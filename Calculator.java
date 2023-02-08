b15db32ffdf58a28ebcea08f001a12cabfc417d1
jacobhogrefe <jhogrefe03@gmail.com>
2023-02-07 19:18:27 -0500

    calculator now adds and subtracts 2 given numbers

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,17 @@ public class Calculator {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter 2 whole numbers separated by a space");
         String[] numbers = input.nextLine().split(" ");
-        int answer = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
-        System.out.println("The answer is " + answer);
+        int[] answer = new int[]{Integer.parseInt(numbers[0]),Integer.parseInt(numbers[1])};
+        System.out.println("Would you like to add or subtract each number?");
+        String function = input.nextLine();
+        if (function.equalsIgnoreCase("add")) {
+            int printAnswer = answer[0] + answer[1];
+            System.out.println("The answer is " + printAnswer);
+        } else if (function.equalsIgnoreCase("subtract")) {
+            int printAnswer = answer[0] - answer[1];
+            System.out.println("The answer is " + printAnswer);
+        } else {
+            System.out.println("Unknown function!");
+        }
     }
 }
