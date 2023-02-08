3164647e64dd0567cfec6b9714548a85f51f9ba8
jacobhogrefe <jhogrefe03@gmail.com>
2023-02-07 19:12:06 -0500

    addition function

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Jawn");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 2 whole numbers separated by a space");
+        String[] numbers = input.nextLine().split(" ");
+        int answer = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+        System.out.println("The answer is " + answer);
     }
 }
